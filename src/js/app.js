51403964b744bb2c9eb04579ef998ad5b7d963d7
carlacentenor <carlacentenor@gmail.com>
2018-11-10 17:58:52 -0500

    cambio atributo name pizzas

--- src/js/app.js
+++ src/js/app.js
@@ -6,50 +6,52 @@ const titleFavoriteSelection = document.querySelector('#title-favorite-selection
 const borderPreview = document.querySelectorAll('.border-preview');
 
 const resetBorderGrey = () => {
-  for (let i = 0; i < borderPreview.length; i += 1) {
-    borderPreview[i].classList.add('pizza__border--desactive');
-    borderPreview[i].classList.remove('pizza__border');
-  }
+    for (let i = 0; i < borderPreview.length; i += 1) {
+        borderPreview[i].classList.add('pizza__border--desactive');
+        borderPreview[i].classList.remove('pizza__border');
+    }
 };
 
 const activeBorderRadioButtonPizza = (id) => {
-  resetBorderGrey();
-  document.querySelector(`#${id}`).classList.remove('pizza__border--desactive');
-  document.querySelector(`#${id}`).classList.add('pizza__border');
+    resetBorderGrey();
+    document.querySelector(`#${id}`).classList.remove('pizza__border--desactive');
+    document.querySelector(`#${id}`).classList.add('pizza__border');
 };
 
 const showSectionPizzasFavorites = (count) => {
-  if (count === '1') {
-    boxPizzaFavorite.classList.add('pizza--hide');
-    titleFavoriteSelection.textContent = 'tu pizza';
-  } else {
-    boxPizzaFavorite.classList.remove('pizza--hide');
-    titleFavoriteSelection.textContent = 'tu primera pizza';
-  }
+    if (count === '1') {
+        boxPizzaFavorite.classList.add('pizza--hide');
+        titleFavoriteSelection.textContent = 'tu pizza';
+    } else {
+        boxPizzaFavorite.classList.remove('pizza--hide');
+        titleFavoriteSelection.textContent = 'tu primera pizza';
+    }
 };
 
 const moveToConfirm = () => {
-  $('html,body').animate({ scrollTop: document.body.scrollHeight }, 1500);
+    $('html,body').animate({ scrollTop: document.body.scrollHeight }, 1500);
 };
 
 
 // get Value of Radio Button
 const getValueRadio = (name) => {
-  const size = document.querySelector(`input[name="${name}"]:checked`).value;
-  return size;
+    const size = document.querySelector(`input[name="${name}"]:checked`).value;
+    return size;
 };
 
 // function that hides pizzas
 const viewPizzas = () => {
-  const valueSelection = getValueRadio('count');
-  showSectionPizzasFavorites(valueSelection);
+    const valueSelection = getValueRadio('count');
+    showSectionPizzasFavorites(valueSelection);
 };
 
 const viewSelectPizza = (event) => {
-  const idElementBorder = event.target.nextElementSibling.firstElementChild.id;
-  activeBorderRadioButtonPizza(idElementBorder);
-  // Mover Scroll
-  moveToConfirm();
+    const idElementBorder = event.target.nextElementSibling.firstElementChild.id;
+    activeBorderRadioButtonPizza(idElementBorder);
+    let valuePizza = getValueRadio('pizza');
+    console.log(valuePizza)
+        // Mover Scroll
+        // moveToConfirm();
 };
 
 // Eventos
